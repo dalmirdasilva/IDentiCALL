8b67adfca18bf4848c500585c3c6927161bbf676
HardCoder <hardcode@07018ctd.(none)>
2013-11-11 20:07:27 -0200

    sync

--- linux/setup.sh
+++ linux/setup.sh
@@ -4,3 +4,4 @@ sudo apt-get update
 sudo apt-get install firebird2.5-superclassic
 sudo dpkg-reconfigure firebird2.5-superclassic
 sudo apt-get install firebird2.5-dev
+sudo apt-get install librxtx-java
